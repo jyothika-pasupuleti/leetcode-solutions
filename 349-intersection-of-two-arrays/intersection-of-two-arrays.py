38e700f77621ea7a2d9e7e71ea411c4972df0b34
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-07-26 17:05:44 +0530

    Time: 31 ms (8.76%) | Memory: 12.6 MB (29.84%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        l = [num for num in set(nums1) if num in set(nums2)]
+
+        return l
+        
