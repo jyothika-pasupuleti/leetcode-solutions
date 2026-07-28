aeade8c6039b33a8777c4035cae09912466194de
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-07-28 23:24:34 +0530

    Time: 14 ms (21.73%) | Memory: 15.2 MB (6.04%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,22 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # for num in nums:
-        #     if nums.count(num) == 1:
-        #         return num
-
-        result = 0
+        d = {}
         for num in nums:
-            result ^= num
-        return result
-
-        #if nums = [2,2,1,3,1,4]  --> return unique values in the list format
-        # d = {}
-        # for val in nums:
-        #     d[val] = d.get(value,0)+1
-        # for key,value in d:
-        #     if value == 1:
-        #         l.append(key)
-
-
-        
+            d[num] = d.get(num,0) + 1
+        for k,v in d.items():
+            if v == 1:
+                return k
