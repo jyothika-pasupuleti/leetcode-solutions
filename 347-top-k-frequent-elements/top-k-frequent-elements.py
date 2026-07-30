b6999ce1d7654eb3267e15ee0376051f9b43c7a5
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-07-30 18:48:35 +0530

    Time: 7 ms (52.14%) | Memory: 22.7 MB (94.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        l = []
-        unique = set(nums)
-        for num in unique:
-            l.append((nums.count(num),num))
-        print(l)
-        l.sort(reverse = True)
-        print(l)
-        res =[]
-        print(l[0][1]) 
-        for i in range(k):
-            res.append(l[i][1])
-        return res
+        # l = []
+        # unique = set(nums)
+        # for num in unique:
+        #     l.append((nums.count(num),num))
+        # print(l)
+        # l.sort(reverse = True)
+        # print(l)
+        # res =[]
+        # print(l[0][1]) 
+        # for i in range(k):
+        #     res.append(l[i][1])
+        # return res
+
+        d = {}
+        for num in nums:
+            d[num] = d.get(num,0) + 1
+        sorted_ele = sorted(d.items(),key = lambda x:x[1],reverse = True)
+        print(sorted_ele)
+        return [sorted_ele[i][0] for i in range(k)]
