416ee38027e009c2f486cf2d4e2ca3d80db25215
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-08-29 22:46:04 +0530

    Time: 52 ms (31.98%) | Memory: 12.6 MB (91.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,8 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        alphabets = ""
-        for char in s:
-            if char.isalnum():
-                alphabets += char
-        return alphabets.lower() == alphabets.lower()[::-1]
+
+        # alphabets = ""
+        # for char in s:
+        #     if char.isalnum():
+        #         alphabets += char
+        # return alphabets.lower() == alphabets.lower()[::-1]
+
+
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            while left < right and not self.isalphanumeric(s[left]):
+                left += 1
+            while left < right and not self.isalphanumeric(s[right]):
+                right -= 1
+
+            left_char = self.tolower(s[left])
+            right_char = self.tolower(s[right])
+
+            if left_char != right_char:
+                return False
+            
+            left += 1
+            right -= 1
+        return True
+
+
+    def isalphanumeric(self,char):
+        return ('A' <= char <= 'Z' or 'a' <= char <= 'z' or '0' <= char <= '9')
+            
+    def tolower(self,char):
+        if 'A' <= char <= 'Z':
+            return chr(ord(char)+32)
+        return char
