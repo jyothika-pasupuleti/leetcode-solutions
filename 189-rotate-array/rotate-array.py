4ee49d5d44efa0947ccd79b1e095daf52f77858b
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-09-07 19:04:43 +0530

    Time: 160 ms (13.45%) | Memory: 28.2 MB (12.97%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,22 +1,35 @@
 class Solution(object):
+    def reverse(self,nums,i,j):
+        while i < j:
+            nums[i],nums[j] = nums[j],nums[i]
+            i += 1
+            j -= 1
+
     def rotate(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # n = len(nums)
-        # rotations = k % n
-        # for _ in range(rotations):
-        #     last_ele = nums.pop()
-        #     nums.insert(0,last_ele)           raised TLE
+        k = k%len(nums)
+        n = len(nums)
+        self.reverse(nums,n-k,n-1)
+        self.reverse(nums,0,n-k-1)
+        self.reverse(nums,0,n-1)
 
 
+        # rotations = k%len(nums)
+        # i = 0
+        # while i < rotations:
+        #     temp = nums[-1]
+        #     for j in range(len(nums)-2,-1,-1):   # got TLE
+        #         nums[j+1] = nums[j]
+        #     nums[0] = temp
 
-        k = k % len(nums)
+        #     i += 1
+
+
     
-        nums[:] = nums[-k:] + nums[:-k]
-
 
 
         
