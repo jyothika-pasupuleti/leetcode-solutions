b49dd33c4b421df1128d3ebc82f9fe556a8da29b
jyothikapasupuleti <jyothipasupuleti1402@gmail.com>
2026-09-17 19:14:36 +0530

    Time: 51 ms (71.18%) | Memory: 26.9 MB (72.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,24 +4,47 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # d = {}
-        # for i in range(len(nums)):
-        #     d[i] = 1
+        # # d = {}
+        # # for i in range(len(nums)):
+        # #     d[i] = 1
+
+        # if not nums:
+        #     return 0
+
+        # nums.sort()
+        # max_count = 1
+        # curr_count = 1
+
+        # for i in range(1,len(nums)):
+        #     if nums[i] - nums[i-1] == 1:    # [1,2,6,7,8]
+        #         curr_count += 1
+        #     elif nums[i] - nums[i-1] != 0:    # [0,1,1,2]
+        #         curr_count = 1
+        #     if curr_count > max_count:
+        #         max_count = curr_count
+
+        # return max_count                    
+
 
         if not nums:
             return 0
-        nums.sort()
-        max_count = 1
-        curr_count = 1
-        for i in range(1,len(nums)):
-            if nums[i] - nums[i-1] == 1:    # [1,2,6,7,8]
-                curr_count += 1
-            elif nums[i] - nums[i-1] != 0:
-                curr_count = 1
-            if curr_count > max_count:
-                max_count = curr_count
-        return max_count                     # [0,1,1,2]
 
+        num_set = set(nums)
+        max_count = 1
+       
+        for num in num_set:
+            if num - 1 not in num_set:
+                curr_count = 1
+                curr = num
+
+                while curr + 1 in num_set:
+                    curr_count += 1
+                    curr += 1
+
+                if curr_count > max_count:
+                    max_count = curr_count
+
+        return max_count
 
 
 
